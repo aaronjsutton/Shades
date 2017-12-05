b583927fd4859df2158c0ef3d7de8c6d65099a4f
Aaron Sutton <aaronjsutton@icloud.com>
2017-12-05 13:39:39 -0500

    Add project and code

---
+++ Example App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example App
+//
+//  Created by Aaron Sutton on 12/5/17.
+//  Copyright © 2017 Aaron Sutton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ Shades/ShadesView.swift
@@ -0,0 +1,84 @@
+//
+//  Shades.swift
+//  Shades
+//
+//  Created by Aaron Sutton on 12/5/17.
+//  Copyright © 2017 Aaron Sutton. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable open class ShadesView: UIView {
+
+	/// The radius of the view's rounded corners
+	@IBInspectable public var cornerRadius: CGFloat {
+		get {
+			return layer.cornerRadius
+		}
+		set {
+			layer.cornerRadius = newValue
+		}
+	}
+
+	/// The width of the border applied to the view
+	@IBInspectable public var borderWidth: CGFloat {
+		get {
+			return layer.borderWidth
+		}
+		set {
+			layer.borderWidth = newValue
+		}
+	}
+
+	/// The color of the border applied to the views
+	@IBInspectable public var borderColor: UIColor {
+		get {
+			return UIColor(cgColor: layer.borderColor!)
+		}
+		set {
+			layer.borderColor = newValue.cgColor
+		}
+	}
+
+	/// The color of the shadow applied to the view
+	@IBInspectable public var shadowColor: UIColor {
+		get {
+			return UIColor(cgColor: layer.shadowColor!)
+		}
+		set {
+			layer.shadowColor = newValue.cgColor
+		}
+	}
+
+	/// The offet of the shadow in the form (x, y)
+	@IBInspectable public var shadowOffset: CGSize {
+		get {
+			return layer.shadowOffset
+		}
+		set {
+			layer.shadowOffset = newValue
+		}
+	}
+
+	/// The blur of the shadown
+	@IBInspectable var shadowRadius: CGFloat {
+		get {
+			return layer.shadowRadius
+		}
+		set {
+			layer.shadowRadius = newValue
+		}
+	}
+
+	/// The opacity of the shadow applied to the view
+	@IBInspectable var shadowOpacity: Float {
+		get {
+			return layer.shadowOpacity
+		}
+		set {
+			layer.shadowOpacity = newValue
+			layer.masksToBounds = false
+		}
+	}
+}
