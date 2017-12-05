9633efdf6c6e4548ea472cc454548db48cd61777
Aaron Sutton <aaronjsutton@icloud.com>
2017-12-05 13:00:22 -0500

    Initial Commit

---
+++ Shades/Shades.h
@@ -0,0 +1,19 @@
+//
+//  Shades.h
+//  Shades
+//
+//  Created by Aaron Sutton on 12/5/17.
+//  Copyright © 2017 Aaron Sutton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shades.
+FOUNDATION_EXPORT double ShadesVersionNumber;
+
+//! Project version string for Shades.
+FOUNDATION_EXPORT const unsigned char ShadesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shades/PublicHeader.h>
+
+
